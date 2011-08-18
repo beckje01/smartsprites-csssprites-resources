9c7a02c35e8cdd1b9259e2f6386e2b153072a452
Jeff Beck <beck@biworldwide.com>
2011-08-18 00:54:38 -0500

    Basic spriting is now working, it doesn't support spriting on a css bundle yet.. will need to see if it's needed. Overall needs cleanup, polish, and testing.

--- grails-app/resourceMappers/SmartspritesCssspritesResourceMapper.groovy
+++ grails-app/resourceMappers/SmartspritesCssspritesResourceMapper.groovy
@@ -4,6 +4,7 @@
  */
 
 import org.grails.plugin.resource.mapper.MapperPhase
+import org.grails.plugin.resource.ResourceService
 import org.carrot2.labs.smartsprites.SmartSpritesParameters
 import org.carrot2.labs.smartsprites.SpriteBuilder
 import org.carrot2.labs.smartsprites.message.MemoryMessageSink
@@ -13,14 +14,15 @@ import org.carrot2.labs.smartsprites.message.MessageLog
 
 class SmartspritesCssspritesResourceMapper
 {
-  def phase = MapperPhase.GENERATION
-  
+  def phase = MapperPhase.MUTATION
+  def resourceService
+  def grailsApplication
   private static final String SPRITE_CSS_SUFFIX = '___sprite_temp___'
 
   static defaultExcludes = ['**/*.min.css']
   static defaultIncludes = ['**/*.css']
   
-  def map(resource, config) 
+  def map(resource, config)  
   {
 
     if(config?.disable)
@@ -32,41 +34,48 @@ class SmartspritesCssspritesResourceMapper
       return false
     }
 
-
-
-    def filepath = resource?.processedFile?.parentFile.getPath() + '\\' + resource?.processedFile?.name
-
-
-
-    def parameters = new SmartSpritesParameters()
-    parameters.cssFileSuffix = SPRITE_CSS_SUFFIX
-
-    
-    def cssFiles = [filepath]
-
-    parameters.cssFiles = cssFiles
-
-
-    def messageSink = new MemoryMessageSink()
-    def messageLog = new MessageLog(messageSink)
-
-
-    new SpriteBuilder(parameters, messageLog).buildSprites()
-
-    def comparator = Message.MessageLevel.COMPARATOR
-    def minLevel = Message.MessageLevel.IE6NOTICE
-    StringBuilder spriteMessages = new StringBuilder()
-    for (message in messageSink.messages) {
-      if (comparator.compare(message.level, minLevel) >= 0) {
-        spriteMessages.append '\t'
-        spriteMessages.append message.toString()
-        spriteMessages.append '\n'
-      }
+    if(resource?.processedFile?.name.startsWith("bundle-"))
+    {
+      println "Skipping a bundle as the spriting is done on a per css file."
     }
+    else
+    {
+
+      def realFile = grailsApplication.parentContext.getResource(resource.actualUrl).file.getPath()
+
+      def parameters = new SmartSpritesParameters()
+      parameters.cssFileSuffix = ""
+      parameters.rootDir = grailsApplication.parentContext.getResource(resource.actualUrl).file.parentFile.getPath()
+      parameters.outputDir = resource?.processedFile.parentFile?.getPath()
+    
+      def cssFiles = [realFile]
+
+      parameters.cssFiles = cssFiles
 
 
-    println "\nSmartSprite log:\n$spriteMessages"
+      def messageSink = new MemoryMessageSink()
+      def messageLog = new MessageLog(messageSink)
 
+
+      new SpriteBuilder(parameters, messageLog).buildSprites()
+
+      def comparator = Message.MessageLevel.COMPARATOR
+      def minLevel = Message.MessageLevel.IE6NOTICE
+      StringBuilder spriteMessages = new StringBuilder()
+      for (message in messageSink.messages) {
+        if (comparator.compare(message.level, minLevel) >= 0) {
+          spriteMessages.append '\t'
+          spriteMessages.append message.toString()
+          spriteMessages.append '\n'
+        }
+      }
+
+
+      println "\nSmartSprite log:\n$spriteMessages"
+
+
+
+    }
   }
 
 }
