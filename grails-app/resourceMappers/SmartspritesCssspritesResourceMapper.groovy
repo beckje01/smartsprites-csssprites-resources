30e7720122ce7a94842db915fc5c836befc9b50a
Jeff Beck <beck@biworldwide.com>
2011-08-24 00:53:51 -0500

    Fixes so it is correctly creating sprites and making the needed Synthetic resources.
    The system will only correctly generate sprites in the same dir as the css file processed.

--- grails-app/resourceMappers/SmartspritesCssspritesResourceMapper.groovy
+++ grails-app/resourceMappers/SmartspritesCssspritesResourceMapper.groovy
@@ -5,6 +5,7 @@
 
 import org.grails.plugin.resource.mapper.MapperPhase
 import org.grails.plugin.resource.ResourceService
+import org.grails.plugin.resource.ResourceMeta
 import org.carrot2.labs.smartsprites.SmartSpritesParameters
 import org.carrot2.labs.smartsprites.SpriteBuilder
 import org.carrot2.labs.smartsprites.message.MemoryMessageSink
@@ -36,17 +37,21 @@ class SmartspritesCssspritesResourceMapper
 
     if(resource?.processedFile?.name.startsWith("bundle-"))
     {
+      //TODO change to logging
       println "Skipping a bundle as the spriting is done on a per css file."
     }
     else
     {
 
       def realFile = grailsApplication.parentContext.getResource(resource.actualUrl).file.getPath()
+      def output = resource?.processedFile.parentFile?.getPath()
+      def tempoutput = output + File.separator + "spritetemp"
 
       def parameters = new SmartSpritesParameters()
+
       parameters.cssFileSuffix = ""
       parameters.rootDir = grailsApplication.parentContext.getResource(resource.actualUrl).file.parentFile.getPath()
-      parameters.outputDir = resource?.processedFile.parentFile?.getPath()
+      parameters.outputDir = tempoutput
     
       def cssFiles = [realFile]
 
@@ -70,10 +75,43 @@ class SmartspritesCssspritesResourceMapper
         }
       }
 
-
+      //TODO change to logging
       println "\nSmartSprite log:\n$spriteMessages"
 
 
+      //Get all the files generated from  smartsprites
+      def tempdir = new File(tempoutput)
+      def newfiles = tempdir.listFiles()
+      //TODO make this support sprites not just in the same dir as the css or make it safe to use that way automatically
+      //This assumes all files are genearted at one level
+
+
+
+      def expectedurlprefix = resource.originalUrl.minus(resource.processedFile.name)
+
+
+      //For each of the files update the resources as needed
+      newfiles.each
+      { file ->
+        if(file.name == resource?.processedFile?.name)
+        {
+          //This is the orginial file just updated the processed file and copy it over.
+          resource.processedFile = file
+          resource.updateActualUrlFromProcessedFile()
+        }
+        else
+        {
+          // make the images created available as resources
+
+          def fileResource = resourceService.findSyntheticResourceForURI(expectedurlprefix+file.name)
+          if(!fileResource)
+          {
+            fileResource = resourceService.newSyntheticResource(expectedurlprefix+file.name, ResourceMeta)
+            fileResource.processedFile=file
+          }
+        }
+
+      }
 
     }
   }
